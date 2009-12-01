628285c3eb43cd45678eb702fc1516af37690d69
nsandonato <nsandonato>
2009-12-01 21:17:26 +0000

    [296625] [translation] JSPTranslator should handle assignment in JSP expressions

--- tests/org.eclipse.jst.jsp.ui.tests/src/org/eclipse/jst/jsp/ui/tests/contentassist/JSPJavaTranslatorTest.java
+++ tests/org.eclipse.jst.jsp.ui.tests/src/org/eclipse/jst/jsp/ui/tests/contentassist/JSPJavaTranslatorTest.java
@@ -61,7 +61,7 @@ public class JSPJavaTranslatorTest extends TestCase {
 				// translatedCursorPosition, 519, translatedCursorPosition);
 				// assertEquals("translation was incorrect", "testJspString",
 				// translation.substring(519, 532));
-				int cursorStart = translation.indexOf("out.print(\"\"+\n testJspString") + 14;
+				int cursorStart = translation.indexOf("out.print( testJspString );" ) + 14;
 				assertEquals("incorrect cursor position >" + cursorStart, 667, cursorStart);
 			}
 			sm.releaseFromRead();
