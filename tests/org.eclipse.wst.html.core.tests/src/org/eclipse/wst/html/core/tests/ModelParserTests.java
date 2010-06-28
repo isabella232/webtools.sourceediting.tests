e0f4da3feda9bfcfeec64428a282ef084ae06d6b
nsandonato <nsandonato>
2010-06-28 14:25:02 +0000

    [308412] [parser] Is HTML DOM correctly treating implicitly-closed tags?

--- tests/org.eclipse.wst.html.core.tests/src/org/eclipse/wst/html/core/tests/ModelParserTests.java
+++ tests/org.eclipse.wst.html.core.tests/src/org/eclipse/wst/html/core/tests/ModelParserTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2004, 2005 IBM Corporation and others.
+ * Copyright (c) 2004, 2010 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -129,7 +129,7 @@ public class ModelParserTests extends TestSuite {
 		addTest(new TestSuite(CDATASectionTest2.class, "CDATASectionTest2"));
 		addTest(new TestSuite(CDATASectionTest3.class, "CDATASectionTest3"));
 		addTest(new TestSuite(PITest.class, "PITest"));
-		addTest(new TestSuite(OmissibleTest.class, "Ommissible tags test"));
+		addTest(new TestSuite(OmissibleTest.class, "Omissible tags test"));
 
 	}
 }
