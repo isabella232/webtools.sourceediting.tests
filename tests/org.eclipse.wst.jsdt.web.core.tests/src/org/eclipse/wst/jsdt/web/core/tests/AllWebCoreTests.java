3a451db34c8ace9405dcd37fb6aa23da0fbe6e7c
nitind <nitind>
2011-02-02 20:41:36 +0000

    [333901] Need to update JS include path after module core nature installed

--- tests/org.eclipse.wst.jsdt.web.core.tests/src/org/eclipse/wst/jsdt/web/core/tests/AllWebCoreTests.java
+++ tests/org.eclipse.wst.jsdt.web.core.tests/src/org/eclipse/wst/jsdt/web/core/tests/AllWebCoreTests.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2011 IBM Corporation and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     IBM Corporation - initial API and implementation
+ *     
+ *******************************************************************************/
 package org.eclipse.wst.jsdt.web.core.tests;
 
 import junit.framework.Test;
@@ -16,6 +27,7 @@ public class AllWebCoreTests extends TestSuite {
 
 		//$JUnit-END$
 		suite.addTestSuite(TestHtmlTranslation.class);
+		suite.addTestSuite(PathUtilsTests.class);
 		return suite;
 	}
 
